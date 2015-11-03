34d627363644a1455a8a0a68b4305ee5ed6c3c42
santiago <santiago@pengostores.com>
2015-11-03 11:48:48 -0600

    class01

---
+++ app/code/local/Pengo/Class01/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Pengo_Class01_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
---
+++ app/code/local/Pengo/Class01/Model/LatestProducts.php
@@ -0,0 +1,25 @@
+<?php
+
+class Pengo_Class01_Model_LatestProducts extends Mage_Core_Model_Abstract
+{
+    const COLLECTION_SIZE = 5;
+
+    public function getLatestProducts()
+    {
+
+        $productCollection = Mage::getModel('catalog/product')->getCollection();
+        $productCollection->addAttributeToSelect('*')
+            ->setOrder('created_at','DESC');
+
+        $productCollection->getSelect()->limit(self::COLLECTION_SIZE);
+
+        /*foreach($productCollection as $product){
+            $product->setPrice(123);
+            $product->setName('Producto Prueba');
+        }*/
+
+        return $productCollection;
+    }
+
+}
+	 
