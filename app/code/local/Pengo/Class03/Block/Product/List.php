a3275f16b2cda17d9a7b7432482e8e1b0ee6fe37
chanflis <the.chanflis@gmail.com>
2015-11-18 14:59:03 -0500

    class

--- app/code/local/Pengo/Class03/Block/Product/List.php
+++ app/code/local/Pengo/Class03/Block/Product/List.php
@@ -1,11 +1,1 @@
-<?php    
-class Pengo_Class03_Block_Product_List extends Mage_Catalog_Block_Product_List
-{
-
-    public function getTag(){
-        
-        
-        return "OK";
-    }
-
-}
+<?php
---
+++ app/code/local/Pengo/Classvp/Helper/Data.php
@@ -0,0 +1,4 @@
+<?php 
+class Pengo_Classvp_Helper_Data extends Mage_Core_Helper_Abstract{
+	
+}
