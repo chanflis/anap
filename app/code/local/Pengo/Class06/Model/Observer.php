19236d79cc18cffe036ade0f248f13d0b2b7c9f5
santiago <santiago@pengostores.com>
2015-11-19 16:40:41 -0600

    observer

--- app/code/local/Pengo/Class06/Model/Observer.php
+++ app/code/local/Pengo/Class06/Model/Observer.php
@@ -3,7 +3,7 @@ class Pengo_Class06_Model_Observer extends Varien_Event_Observer
 {
     const CUSTOMER_GROUP_COMPRADOR = 6;
 
-    
+
     public function changeGroup()
     {
         $customerModel = Mage::getSingleton('customer/session')->getCustomer();
