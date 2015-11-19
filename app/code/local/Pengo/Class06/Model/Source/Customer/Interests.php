ed880845c48ce847747849931ba68ddf2f090587
santiago <santiago@pengostores.com>
2015-11-18 18:23:40 -0600

    class06 started, installer created

---
+++ app/code/local/Pengo/Class06/Model/Source/Customer/Interests.php
@@ -0,0 +1,36 @@
+<?php
+class Pengo_Class06_Model_Source_Customer_Interests extends Mage_Eav_Model_Entity_Attribute_Source_Abstract
+{
+    public function getAllOptions()
+    {
+        if ($this->_options === null) {
+            $this->_options = array();
+            $this->_options[] = array(
+                    'value' => '',
+                    'label' => 'Elegir opción'
+            );
+            $this->_options[] = array(
+                    'value' => 1,
+                    'label' => 'Deportes'
+            );
+            $this->_options[] = array(
+                    'value' => 2,
+                    'label' => 'Noticias'
+            );
+            $this->_options[] = array(
+                    'value' => 3,
+                    'label' => 'Cultura'
+            );
+            $this->_options[] = array(
+                    'value' => 4,
+                    'label' => 'Política'
+            );
+            $this->_options[] = array(
+                    'value' => 5,
+                    'label' => 'Moda'
+            );
+        }
+ 
+        return $this->_options;
+    }
+}
